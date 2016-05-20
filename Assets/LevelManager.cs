3c8afca8c10b066087a4ee20fa07a417770a011e
JJ Dorko <jdorko90@gmail.com>
2016-05-19 21:55:05 -0400

    initial commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log("Level load for: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitGame() {
+		Debug.Log("Trying to quit!");
+		Application.Quit();
+	}
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	int min = 1;
+	int max = 1000;
+	int guess = 500;
+	int maxGuesses = 5;
+	int currentNumGuesses;
+	
+	public Text guessNumberText;
+	public Text guessRemainingText;
+
+	void Start () {
+		max = max + 1;
+		currentNumGuesses = 0;
+		guessRemainingText.text = "Number of guesses left: " + maxGuesses;
+	}
+	
+	public void Correct(){
+		Application.LoadLevel("Lose");
+	}
+	
+	public void GuessHigher () {
+		min = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower () {
+		max = guess;
+		NextGuess();
+	}
+	
+	void NextGuess() {
+		guess = (max + min) / 2;
+		currentNumGuesses++;
+		if (maxGuesses <= currentNumGuesses) {
+			Application.LoadLevel("Win");
+		}
+		guessNumberText.text = "" + guess;
+		guessRemainingText.text = "Number of guesses left: " + (maxGuesses - currentNumGuesses);
+	}
+}
